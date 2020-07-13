0a1385de2e5d3b3177d117fddd093c786d0d6291
Marco <rubolini.marco@gmail.com>
2020-07-14 00:19:43 +0200

    CRUD completo posts

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/admin', 'HomeController@index')->name('home')->middleware('auth');
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::prefix('admin') ->namespace('Admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('/posts', 'PostController');
+});
