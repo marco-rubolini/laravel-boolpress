1b85a3675e2ff52f427e1e5cfffdea70bc9ae605
Marco <rubolini.marco@gmail.com>
2020-07-15 15:56:11 +0200

    visualizzazione dei post nella categoria

--- resources/views/guest/posts/show.blade.php
+++ resources/views/guest/posts/show.blade.php
@@ -6,6 +6,15 @@
         <div class="col-md-12">
             <h1>{{ $post->title }}</h1>
             <p>{{ $post->content }}</p>
+            <p>Categoria:
+                @if ($post->category)
+                    <a href="{{ route('categories.show', ['slug' => $post->category->slug]) }}">
+                        {{ $post->category->name }}
+                    </a>
+                @else
+                    -
+                @endif
+            </p>
         </div>
     </div>
 </div>
